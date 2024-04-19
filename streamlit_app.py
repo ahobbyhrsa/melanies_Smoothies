79cf5ce818d440f5d30c7d3bd153b1bf2bb303a0
ahobbyhrsa <156927927+ahobbyhrsa@users.noreply.github.com>
2024-04-19 13:34:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Example Streamlit App :cup_with_straw:")
